f61d553f1350ab7a1b44990c2f84f211c97c390c
Maetdol <rndka9@gmail.com>
2020-08-03 15:36:29 +0000

    Add #000 - Complete main functions of file manager Ended functions: previousLine, nextLine, search Create file: explorManager

--- explore-kakaochat/fileManager.js
+++ explore-kakaochat/fileManager.js
@@ -7,6 +7,14 @@ class FileManager {
     this._cursor = -1;
   }
 
+  decodeUint8( array ) {
+    return this.textDecoder.decode( array );
+  }
+
+  sizeof( str ) {
+    return new Blob([ str ]).size;
+  }
+
   set cursor( pos ) {
     this._cursor = pos;
   }
@@ -33,45 +41,45 @@ class FileManager {
     return { end, array: array.slice( 0, end ) };
   }
 
-  // TODO: REFACTORING
-  // should support regex
-  // filtering for date, user
-  // multiple result
+  async readBySize( size ) {
+    const buffer = await this.sliceFile( this.cursor + size );
+    const uint8Array = new Uint8Array( buffer );
+    const isEmpty = uint8Array.length === 0;
+    if( isEmpty ) {
+      return null;
+    }
+    return uint8Array;
+  }
+
   async search( query ) {
-    const queryLength = query.length || query.source.length;
-    let i=0;
     while( true ) {
-      const buffer = await this.sliceFile( this.cursor + this.MAX_CHUNK );
-      const uint8Array = new Uint8Array( buffer );
-      const isEmpty = uint8Array.length === 0;
-      if( isEmpty ) {
+      const uint8Array = await this.readBySize( this.MAX_CHUNK );
+      if( uint8Array === null ) {
         return null;
       }
-      const str = this.textDecoder.decode( uint8Array );
-      const result = str.indexOf( query );
-      if( result > -1 ) {
-        const position = new Blob([ str.slice( 0, result ) ]).size;
-        this.cursor += position;
-        return { result: str.slice(result, result + 300), position: this.cursor, query };
+      const stringify = this.textDecoder.decode( uint8Array );
+      const queryIndex = stringify.indexOf( query );
+      if( queryIndex > -1 ) {
+        const uselessString = stringify.slice( 0, queryIndex );
+        const resultPosition = this.sizeof( uselessString ) + this.cursor;
+        this.cursor = resultPosition + this.sizeof( query );
+        return { 
+          position: resultPosition,
+          query 
+        };
       }
-      if( i++ > 450 ) {
-        console.log( buffer, query, this.cursor );
-        return false;
-      }
-      this.cursor += this.MAX_CHUNK - queryLength;
+      this.cursor += this.MAX_CHUNK - query.length;
     }
   }
 
-  async readline() {
+  async nextLine() {
     
     let buffers = [];
     let i = 0;
     while( true ) {
       
-      const buffer = await this.sliceFile( this.cursor + this.BUF_SIZE );
-      const uint8Array = new Uint8Array( buffer );
-      const isEmpty = uint8Array.length === 0;
-      if( isEmpty ) {
+      const uint8Array = await this.readBySize( this.BUF_SIZE ); 
+      if( uint8Array === null ) {
         return null;
       }
 
@@ -94,8 +102,42 @@ class FileManager {
       i++;
     }
 
-    const blob = new Blob( buffers );
-    return blob.text();
+    return new Uint8Array( ...buffers );
+  }
+
+  lastIndexOf( array, value, start ) {
+    let i = start || array.length - 1;
+    while( i >= 0 ) {
+      if( array[i] === value ) {
+        return i;
+      }
+      i--;
+    }
+    return -1;
+  }
+
+  async previousLine() {
+    
+    let buffers = [];
+    let i = 0;
+    while( this.cursor >= 0 ) {
+
+      this.cursor -= this.BUF_SIZE;
+      const uint8Array = await this.readBySize( this.BUF_SIZE );
+      if( uint8Array === null ) {
+        return null;
+      }
+      const lastIndex = this.lastIndexOf( uint8Array, this.NEW_LINE );
+      if( lastIndex != -1 ) {
+        let cursor = this.cursor;
+        this.cursor += lastIndex + 1;
+        const prevLine = await this.nextLins();
+        this.cursor = cursor + lastIndex;
+        return prevLine;
+      }
+    }
+
+    return null;
   }
 
 }
