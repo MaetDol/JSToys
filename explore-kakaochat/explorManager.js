bf496bbb82340e763af509363c68585269a818e8
Maetdol <rndka9@gmail.com>
2020-08-05 14:29:55 +0000

    Fix mobile query generator and
    Change parameterName 'isRegExp' to 'useRegExp' more understandable
    Change RegExp that not applied
    Added method 'search' that only search one and start from current cursor
    Make cancelable while searching it for user interaction
    
    Added readlines( n, doReverse, cursorPosition ) and several methods for that method.
    It reads n lines from cursorPosition, direction to next or previously.
    
    Added getWrappedLines( n, cursorPosition ). It read above and below n lines from cursorPosition.
    It runs based on readlines

--- explore-kakaochat/explorManager.js
+++ explore-kakaochat/explorManager.js
@@ -1,6 +1,7 @@
 class ExplorManager {
   constructor( fileManager ) {
     this.fileManager = fileManager;
+    this.exitSearch = false;
   }
 
   set fileManager( fileManager ) {
@@ -12,26 +13,26 @@ class ExplorManager {
   }
 
   escapeRegExp( str ) {
-    return str.replace(/[.*+\-?^${}()|[\]\\\/]/g, '\\$&');
+    return str.replace(/[.*+\-?^${}()|[\]\\\/]/g, '\\$&').replace(/\n/g,'\\n');
   }
 
-  generateMobileQuery({ timestamp={}, user='', chat='', isRegExp=false }={}) {
+  generateMobileQuery({ timestamp={}, user='', chat='', useRegExp=false }={}) {
     let {
       year = '\\d{4}', 
       month = '\\d{1,2}', 
       date = '\\d{1,2}',
       amOrPm = '오(전|후)',
       hour = '\\d{1,2}',
-      minute = '\\d{1,2}',
+      minute = '\\d{2}',
     } = timestamp;
 
-    if( !isRegExp ) {
+    if( !useRegExp ) {
       chat = this.escapeRegExp( chat );
     }
     if( user ) {
       user = this.escapeRegExp( user );
     } else {
-      user = '[^:\n]+';
+      user = '[^:\\n]+';
     }
 
     return new RegExp(
@@ -41,10 +42,11 @@ class ExplorManager {
   }
   
   async searchAll( query ) {
+    this.fileManager.cursor = 0;
     let results = [];
     while( true ) {
       const matched = await this.fileManager.search( query );
-      const isEnd = matched === null;
+      const isEnd = matched === null || this.exitSearch;
       if( isEnd ) {
         return results;
       }
@@ -52,11 +54,70 @@ class ExplorManager {
     }
   }
 
+  async search( query ) {
+    return await this.fileManager.search( query );
+  }
+
   indexOf( query ) {
     return this.fileManager.search( query );
   }
 
-  async searchChat( chat ) {
-    const fm = this.fileManager;
+  async readlines( n, isReverse, cursor=null ) {
+    this.fileManager.cursor = cursor || this.fileManager.cursor;
+    let index = 0;
+    let readline = this.fileManager.nextLine.bind( this.fileManager );
+    let isOutOfIndex = i => i >= n;
+    let nextIndex = i => i +1;
+
+    if( isReverse ) {
+      readline = this.fileManager.previousLine.bind( this.fileManager );
+      isOutOfIndex = i => i < 0;
+      index = n -1;
+      nextIndex = i => i -1;
+    }
+
+    const lines = [];
+    while( !isOutOfIndex( index ) ) {
+      const line = await readline();
+      if( line === null ) {
+        return lines;
+      }
+      lines[index] = line;
+      index = nextIndex( index );
+    }
+    return this.decodeLines( lines );
   }
+
+  async getPreviousLines( n, cursor ) {
+    return await this.readlines( n, true, cursor );
+  }
+
+  async getNextLines( n, cursor ) {
+    return await this.readlines( n, false, cursor );
+  }
+
+  async getWrappedLines( n, cursor=this.fileManager.cursor ) {
+
+    const previousLines = await this.getPreviousLines( n, cursor );
+    const startCursor = this.fileManager.cursor;
+
+    const nextLines = await this.getNextLines( n, cursor );
+    const endCursor = this.fileManager.cursor;
+
+    this.fileManager.cursor = cursor;
+
+    return {
+      previousLines,
+      nextLines,
+      cursor: {
+        start: startCursor,
+        end: endCursor,
+      }
+    };
+  }
+
+  decodeLines( lines ) {
+    return lines.map( l => this.fileManager.decodeUint8( l ) );
+  }
+
 }
