2408d6c3cca2195b8fa2c46606d36d76f4d95b3d
Maetdol <rndka9@gmail.com>
2020-12-20 20:47:36 +0900

    Apply Character size checkboard
    Using css and font metrics

--- ascii-flowchart/scripts/index.mjs
+++ ascii-flowchart/scripts/index.mjs
@@ -1,9 +1,10 @@
 import Box from './Ascii/Box.mjs';
 import CharacterMetrics from './utils/CharacterMetrics.mjs';
 
-const characterMetrics = new CharacterMetrics('Consolas');
-console.log( characterMetrics.getHeight() );
-
 const asciiBox = new Box({w:35, h:5});
 const editor = document.getElementById('editor').children[0];
 editor.innerText = asciiBox.render();
+
+const characterMetrics = new CharacterMetrics('Consolas');
+const height = characterMetrics.getHeight() + 'px';
+editor.style.setProperty('--height', height );
