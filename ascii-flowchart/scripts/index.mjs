51c42410e0ce73e9d33448f28c7ce69d2d385d53
Maetdol <rndka9@gmail.com>
2020-12-20 23:15:25 +0900

    Add todo
    ..in source code

--- ascii-flowchart/scripts/index.mjs
+++ ascii-flowchart/scripts/index.mjs
@@ -8,3 +8,11 @@ editor.innerText = asciiBox.render();
 const characterMetrics = new CharacterMetrics('Consolas');
 const height = characterMetrics.getHeight() + 'px';
 editor.style.setProperty('--height', height );
+
+/*
+  Renderer 따로 만들기?
+  배열로 통쨰로 가지고 있고 바뀌는 부분 수정. 매번 join을 해야하나? 성능 문제
+  마우스 호버? 오브젝트 클릭?
+  영어가 아닌 문자?(너비가 1ch가 아님)
+  Ascii resizing
+*/
