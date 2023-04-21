d143e2f4be88d4d530e0126857692a10e30fc961
MaetDol <rndka9@gmail.com>
2023-04-22 01:37:13 +0900

    포매팅

--- water-wave/scripts/index.mjs
+++ water-wave/scripts/index.mjs
@@ -1,6 +1,6 @@
-import { Shape, Line, SubLine, Dot, Bubble, } from './Shapes/all.mjs';
-import TaskQueue, { Task } from './TaskQueue.mjs';
 import Renderer from './Renderer.mjs';
+import { Bubble, Dot, Line, Shape, SubLine } from './Shapes/all.mjs';
+import TaskQueue, { Task } from './TaskQueue.mjs';
 
 const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
@@ -9,8 +9,8 @@ const ctx = canvas.getContext('2d');
 
 const endX = canvas.width + 200;
 const halfHeight = canvas.height / 2;
-const startDot = _=> new Dot({id:-1, x:-200, y:halfHeight, r:4});
-const endDot = _=> new Dot({id:-2, x:endX, y:halfHeight, r:4});
+const startDot = (_) => new Dot({ id: -1, x: -200, y: halfHeight, r: 4 });
+const endDot = (_) => new Dot({ id: -2, x: endX, y: halfHeight, r: 4 });
 
 const line = new Line({
   dot1: startDot(),
@@ -37,51 +37,51 @@ const sub2 = new SubLine({
   dot2: endDot(),
   friction: 0.9,
   dotDistance: 50,
-  color:'#ffff217f', 
+  color: '#ffff217f',
   parent: sub1,
   weight: 0.7,
   height: halfHeight,
 });
 
 const cursor = new (class extends Dot {
-  conflict( dot ) {
-    const {x, y, r} = this;
-    const distance = Math.sqrt((dot.x - x)**2 + (dot.y - y)**2);
+  conflict(dot) {
+    const { x, y, r } = this;
+    const distance = Math.sqrt((dot.x - x) ** 2 + (dot.y - y) ** 2);
     return distance < r;
   }
-})({id:-3, x:-50, y:-50, r:20, color:'#00000088'});
+})({ id: -3, x: -50, y: -50, r: 20, color: '#00000088' });
 
 const duck = new (class extends Shape {
-  constructor() { 
-    super(); 
+  constructor() {
+    super();
     const img = document.getElementById('duck');
-    this.props = { img }; 
+    this.props = { img };
   }
-  draw( context ) {
-    const {x, y, rotation, img } = this.props;
-    const { width:w, height:h } = img;
+  draw(context) {
+    const { x, y, rotation, img } = this.props;
+    const { width: w, height: h } = img;
 
     context.save();
     context.strokeStyle = 'black';
     context.translate(x, y);
-    context.rotate( rotation );
-    context.drawImage( img, -w/2, -h*0.9 );
+    context.rotate(rotation);
+    context.drawImage(img, -w / 2, -h * 0.9);
     context.restore();
   }
   update() {}
   collision() {}
 })();
-line.floating( duck, 0.8 );
+line.floating(duck, 0.8);
 
 const waveText = new (class extends Shape {
-  draw( context ) {
+  draw(context) {
     context.fillStyle = '#494B4D';
     context.font = '120px "Bebas Neue"';
     let lastX = 120;
     const top = halfHeight + 60;
-    [...'WAVE'].forEach( ch => {
-      const text = context.measureText( ch );
-      context.fillText( ch, lastX, top );
+    [...'WAVE'].forEach((ch) => {
+      const text = context.measureText(ch);
+      context.fillText(ch, lastX, top);
       lastX += text.width + 25;
     });
   }
@@ -89,7 +89,7 @@ const waveText = new (class extends Shape {
   collision() {}
 })();
 
-const bubble = new class {
+const bubble = new (class {
   constructor({ start, end, bottom }) {
     this.group = [];
     this.queue = new TaskQueue();
@@ -105,79 +105,83 @@ const bubble = new class {
 
   filtering() {
     const group = this.group;
-    for( let i=group.length-1; i >= 0; i-- ) {
+    for (let i = group.length - 1; i >= 0; i--) {
       const bubble = group[i];
-      if( bubble.y < 0 ) group.splice( i, 1 );
+      if (bubble.y < 0) group.splice(i, 1);
     }
   }
 
   bubbling() {
     this.filtering();
-    const counts = Math.ceil( Math.random() * 6 ) + 2;
+    const counts = Math.ceil(Math.random() * 6) + 2;
     const centerX = this.start + Math.random() * this.distance;
-    for( let i=0; i < counts; i++ ) {
+    for (let i = 0; i < counts; i++) {
       const r = 10 * Math.random() + 4;
-      const x = centerX + Math.random() * this.range - this.range/2;
-      const y = this.bottom + Math.random() * this.range * (20/r);
-      const alpha = Math.ceil( Math.random() * (255*0.6) + (255*0.2) );
-      this.group.push( new Bubble({
-        x, y, r,
-        color: '#FFFFFF' + alpha.toString(16),
-      }));
+      const x = centerX + Math.random() * this.range - this.range / 2;
+      const y = this.bottom + Math.random() * this.range * (20 / r);
+      const alpha = Math.ceil(Math.random() * (255 * 0.6) + 255 * 0.2);
+      this.group.push(
+        new Bubble({
+          x,
+          y,
+          r,
+          color: '#FFFFFF' + alpha.toString(16),
+        })
+      );
     }
-    this.queue.add( new Task({
-      job: () => this.bubbling(),
-      delay: 2000 + 500 * Math.random()
-    }));
+    this.queue.add(
+      new Task({
+        job: () => this.bubbling(),
+        delay: 2000 + 500 * Math.random(),
+      })
+    );
   }
-}({ start: line.props.start.x, end: line.props.end.x, bottom: canvas.height });
+})({ start: line.props.start.x, end: line.props.end.x, bottom: canvas.height });
 
-const renderer = new Renderer( 
-  canvas.width, canvas.height, ctx, 
-  [
-    [waveText],
-    [sub1, sub2],
-    [line, cursor], 
-    bubble.group,
-  ], 
-);
+const renderer = new Renderer(canvas.width, canvas.height, ctx, [
+  [waveText],
+  [sub1, sub2],
+  [line, cursor],
+  bubble.group,
+]);
 renderer.render();
 
-canvas.addEventListener('mousemove', e => {
+canvas.addEventListener('mousemove', (e) => {
   cursor.x = e.clientX;
   cursor.y = e.clientY;
 });
 
-window.addEventListener('resize', e => {
+window.addEventListener('resize', (e) => {
   const w = e.target.innerWidth;
   const h = e.target.innerHeight;
   canvas.width = renderer.width = w;
   canvas.height = renderer.height = h;
 });
 
-
-function waveLoop( line, queue ) {
-  const waveTask = ( dot, delay ) => {
+function waveLoop(line, queue) {
+  const waveTask = (dot, delay) => {
     return new Task({
-      job: () => dot.v -= 8,
+      job: () => (dot.v -= 8),
       delay,
     });
   };
 
   const wave = () => {
-    const {dots} = line.props;
-    dots.forEach((d, i) => queue.add( waveTask(d, 60) ));
+    const { dots } = line.props;
+    dots.forEach((d, i) => queue.add(waveTask(d, 60)));
   };
 
-  queue.add( new Task({
-    job: () => {
-      wave();
-      waveLoop( line, queue );
-    },
-    delay: 8000,
-  }));
+  queue.add(
+    new Task({
+      job: () => {
+        wave();
+        waveLoop(line, queue);
+      },
+      delay: 8000,
+    })
+  );
 }
 
 const queue = new TaskQueue();
-waveLoop( line, queue );
+waveLoop(line, queue);
 queue.consume();
