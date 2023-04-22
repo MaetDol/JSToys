659d384db2c53bdec80c6d18b5e542fef8181b9b
MaetDol <rndka9@gmail.com>
2023-04-23 02:55:00 +0900

    [#29] 오리 클릭 이벤트 추가
    일반 사각형이 아닌, 회전하는 사각형이다보니 여러 로직이 들어갔네요
    
    회전하는 point 는 삼각함수로 처리했고, 폴리곤 형태는 레이캐스팅으로 처리했습니당
    
    우선은 네모나게 만들었어용

--- water-wave/scripts/index.mjs
+++ water-wave/scripts/index.mjs
@@ -98,8 +98,14 @@ const duck = new (class extends Shape {
   constructor() {
     super();
     const img = document.getElementById('duck');
-    this.props = { img };
+    this.props = {
+      img,
+      rotation: 0,
+      x: 0,
+      y: 0,
+    };
   }
+
   draw(context) {
     const { x, y, rotation, img } = this.props;
     const { width: w, height: h } = img;
@@ -111,6 +117,35 @@ const duck = new (class extends Shape {
     context.drawImage(img, -w / 2, -h * 0.9);
     context.restore();
   }
+
+  isDuckArea(x, y) {
+    const { img, x: centerX, y: centerY, rotation } = this.props;
+    const { width, height } = img;
+
+    const origin = {
+      x: centerX,
+      y: centerY,
+    };
+
+    const left = centerX - width / 2;
+    const top = centerY - height;
+
+    // left-top
+    const lt = getRotatedPoint({ x: left, y: top }, origin, rotation);
+    // left-bottom
+    const lb = getRotatedPoint({ x: left, y: top + height }, origin, rotation);
+    // right-top
+    const rt = getRotatedPoint({ x: left + width, y: top }, origin, rotation);
+    // right-bottom
+    const rb = getRotatedPoint(
+      { x: left + width, y: top + height },
+      origin,
+      rotation
+    );
+
+    return checkInsideUsingRayCasting([lt, rt, rb, lb], { x, y });
+  }
+
   update() {}
   collision() {}
 })();
@@ -200,6 +235,13 @@ canvas.addEventListener('mousemove', (e) => {
   resetCursorPrev();
 });
 
+canvas.addEventListener('click', (e) => {
+  const isDuckClicked = duck.isDuckArea(e.clientX, e.clientY);
+  if (!isDuckClicked) return;
+
+  console.log('-----------------QUAK!');
+});
+
 window.addEventListener('resize', (e) => {
   const w = e.target.innerWidth;
   const h = e.target.innerHeight;
@@ -276,4 +318,41 @@ function threshold(value, maxLimit, minLimit) {
   if (value > maxLimit) return maxLimit;
   if (value < minLimit) return minLimit;
   return value;
+}
+
+function getRotatedPoint(point, originPoint, radian) {
+  const x = point.x - originPoint.x;
+  const y = point.y - originPoint.y;
+
+  const hypo = Math.sqrt(x ** 2 + y ** 2);
+  const theta = Math.atan2(y, x);
+
+  const movedX = originPoint.x + hypo * Math.cos(radian + theta);
+  const movedY = originPoint.y + hypo * Math.sin(radian + theta);
+
+  return { x: movedX, y: movedY };
+}
+
+function checkInsideUsingRayCasting(polygon, { x, y }) {
+  let intersections = 0;
+
+  for (let j = -1, i = 0; i < polygon.length; i++, j++) {
+    const a = polygon.at(j);
+    const b = polygon[i];
+
+    // 세로 선상으로 두 점의 사이에 있다면, 수평으로 직선을 그어
+    // 충돌하는지 확인할 수 있을 것이다
+    const isAcrossY = a.y > y !== b.y > y;
+    if (!isAcrossY) continue;
+
+    // 확인하려는 좌표 기준 수평선을 그어, 두 점으로 이어진 직선에
+    // 교차하는 점이 있는지 확인한다
+    const vertexSlope = (a.x - b.x) / (a.y - b.y);
+    const acrossXCoordinate = vertexSlope * (y - b.y) + b.x;
+
+    // 양쪽 다 확인 할 필요 없이, 한 쪽(오른쪽) 만 확인해도 판별할 수 있다
+    if (x < acrossXCoordinate) intersections++;
+  }
+
+  return intersections % 2 !== 0;
 }
