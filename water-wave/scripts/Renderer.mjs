1ac97b1f5d990f0a73349936b64c957ae36ff41b
MaetDol <rndka9@gmail.com>
2024-07-28 00:23:32 +0900

    [#40] water-wave 최대 프레임 60으로 제한
    주사율이 높은 환경에선 물리 계산도 그만큼 빨라져 두배속으로 애니메이션이
    재생되는 느낌이었어요.
    
    작업하는 환경인 60프레임 제한으로 보이게끔 수정했습니다.
    
    아니면 뭔가 물리 계산하는 곳에서 주사율에 따라 가중치를 다르게 계산해야
    할 것 같은데... 흠...
    
    close #40

--- water-wave/scripts/Renderer.mjs
+++ water-wave/scripts/Renderer.mjs
@@ -1,32 +1,54 @@
 class Renderer {
-	#shapes;
-	constructor(w, h, context, layers) {
-		this.layers = layers;
-		this.ctx = context;
-		this.w = w;
-		this.h = h;
-	}
+  #shapes;
+  #FRAME_RATE = 60;
+  #FRAME_INTERVAL = 1000 / this.#FRAME_RATE;
+  #lastFrameTimestamp = null;
+  fps = 0;
 
-	set width(w) {
-		this.w = w;
-	}
-	set height(h) {
-		this.h = h;
-	}
+  constructor(w, h, context, layers) {
+    this.layers = layers;
+    this.ctx = context;
+    this.w = w;
+    this.h = h;
+  }
 
-	render() {
-		this.ctx.clearRect(0, 0, this.w, this.h);
+  set width(w) {
+    this.w = w;
+  }
+  set height(h) {
+    this.h = h;
+  }
 
-		this.layers.forEach((layer) => {
-			layer.forEach((s) => {
-				s.draw(this.ctx);
-				s.collision(layer);
-			});
+  render(timestamp) {
+    if (!this.#lastFrameTimestamp) {
+      this.#lastFrameTimestamp = timestamp;
+    }
 
-			layer.forEach((s) => s.update());
-		});
-		requestAnimationFrame((_) => this.render());
-	}
+    const interval = timestamp - this.#lastFrameTimestamp;
+    if (interval < this.#FRAME_INTERVAL) {
+      this.loopRender();
+      return;
+    }
+
+    this.ctx.clearRect(0, 0, this.w, this.h);
+
+    this.layers.forEach((layer) => {
+      layer.forEach((s) => {
+        s.draw(this.ctx);
+        s.collision(layer);
+      });
+
+      layer.forEach((s) => s.update());
+    });
+
+    this.fps = 1000 / interval;
+    this.#lastFrameTimestamp = timestamp;
+    this.loopRender();
+  }
+
+  loopRender() {
+    requestAnimationFrame((timestamp) => this.render(timestamp));
+  }
 }
 
 export default Renderer;
